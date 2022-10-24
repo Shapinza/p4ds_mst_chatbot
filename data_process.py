f84810c4dfa859d18c6c58799e19c2e23fe51fac
Shapinza <zhzchen327@zohomail.com>
2022-10-24 16:53:57 +0800

    fixed bug

--- data_process.py
+++ data_process.py
@@ -12,56 +12,56 @@ def data_process(intent, philosopher_name):
     with open(intent) as file:
         data = json.load(file)
 
-    try:
-        with open(philosopher_name + "_data.pickle", "rb") as f:
-            words, labels, training, output = pickle.load(f)
-    except:
-        words = []
-        labels = []
-        docs_x = []
-        docs_y = []
+    # try:
+    #     with open(philosopher_name + "_data.pickle", "rb") as f:
+    #         words, labels, training, output = pickle.load(f)
+    # except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
 
-        for intent in data["intents"]:
-            for pattern in intent["patterns"]:
-                wrds = nltk.word_tokenize(pattern)
-                words.extend(wrds)
-                docs_x.append(wrds)
-                docs_y.append(intent["tag"])
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
 
-            if intent["tag"] not in labels:
-                labels.append(intent["tag"])
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
 
-        words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-        words = sorted(list(set(words)))
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
 
-        labels = sorted(labels)
+    labels = sorted(labels)
 
-        training = []
-        output = []
+    training = []
+    output = []
 
-        out_empty = [0 for _ in range(len(labels))]
+    out_empty = [0 for _ in range(len(labels))]
 
-        for x, doc in enumerate(docs_x):
-            bag = []
+    for x, doc in enumerate(docs_x):
+        bag = []
 
-            wrds = [stemmer.stem(w.lower()) for w in doc]
+        wrds = [stemmer.stem(w.lower()) for w in doc]
 
-            for w in words:
-                if w in wrds:
-                    bag.append(1)
-                else:
-                    bag.append(0)
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
 
-            output_row = out_empty[:]
-            output_row[labels.index(docs_y[x])] = 1
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
 
-            training.append(bag)
-            output.append(output_row)
+        training.append(bag)
+        output.append(output_row)
 
-        training = numpy.array(training)
-        output = numpy.array(output)
+    training = numpy.array(training)
+    output = numpy.array(output)
 
-        with open(philosopher_name + "_data.pickle", "wb") as f:
-            pickle.dump((words, labels, training, output), f)
+    with open(philosopher_name + "_data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
 
     return [words, labels, training, output, data]
